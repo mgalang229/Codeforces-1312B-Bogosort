4d29102046c09c438121c58be101d9d9decdb43d
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-04-07 13:20:52 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	private static FastReader in;
+	
+	public static void main(String[] args) {
+		in = new FastReader();
+		int tc = 1;
+		tc = in.nextInt();
+		for(int t = 0; t < tc; t++) {
+			new Solver();
+		}
+	}
+	
+	static class Solver { 
+		Solver() {
+			
+			// input
+			int n = in.nextInt();
+			Integer[] a = new Integer[n];
+			for(int i = 0; i < n; i++) {
+				a[i] = in.nextInt();
+			}
+			
+			// simply sort the sequence in non-increasing order
+			Arrays.sort(a, Collections.reverseOrder());
+			for(int i = 0; i < n; i++) {
+				System.out.print(a[i] + " ");
+			}
+			
+			// output
+			System.out.println();
+		}
+	}
+	
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		
+		String next() {
+			while(st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
